e8715ebdbb374b52f028e3d5afa09a8134ff5960
vlddev <volodymyr.vlad@yahoo.com>
2022-04-15 14:03:33 +0200

    rewrite decompiled

--- src/main/java/vlad/aligner/wuo/MatchSentence.java
+++ src/main/java/vlad/aligner/wuo/MatchSentence.java
@@ -6,52 +6,44 @@ import java.util.List;
 import java.util.Locale;
 
 public class MatchSentence {
-   private Locale lang;
-   private List<MatchWf> matchWfList = new ArrayList<MatchWf>();
+    private Locale lang;
+    private List<MatchWf> matchWfList = new ArrayList<MatchWf>();
 
-   public MatchSentence(String sentStr, Locale lang, TranslatorInterface translator) {
-      this.lang = lang;
-      Sentence sent = new Sentence(sentStr);
-      Iterator var5 = sent.getElemList().iterator();
-
-      while(var5.hasNext()) {
-         String wf = (String)var5.next();
-         this.matchWfList.add(new MatchWf(wf, translator.getBaseForm(wf, lang)));
-      }
-
-   }
-
-   public List getMatchWfList() {
-      return this.matchWfList;
-   }
-
-   public List findMatches(List words) {
-      List ret = new ArrayList();
-      Iterator var3 = this.matchWfList.iterator();
-
-      while(var3.hasNext()) {
-         MatchWf mwf = (MatchWf)var3.next();
-         if (mwf.matchingWf == null && mwf.bases.stream().anyMatch((element) -> {
-            return words.contains(element);
-         })) {
-            ret.add(mwf);
-         }
-      }
-
-      return ret;
-   }
-
-   class MatchWf {
-    String wf;
-    List bases;
-    MatchWf matchingWf;
-    Word base;
-    Word trBase;
- 
-    MatchWf(String wf, List bases) {
-       this.matchingWf = null;
-       this.wf = wf;
-       this.bases = bases;
+    public MatchSentence(String sentStr, Locale lang, TranslatorInterface translator) {
+        this.lang = lang;
+        Sentence sent = new Sentence(sentStr);
+        for (String wf : sent.getElemList()) {
+            this.matchWfList.add(new MatchWf(wf, translator.getBaseForm(wf, lang)));
+        }
     }
- }
+
+    public List<MatchWf> getMatchWfList() {
+        return this.matchWfList;
+    }
+
+    public List<MatchWf> findMatches(List words) {
+        List<MatchWf> ret = new ArrayList();
+        for (MatchWf mwf : matchWfList) {
+            if (mwf.matchingWf == null && mwf.bases.stream().anyMatch((element) -> {
+                return words.contains(element);
+            })) {
+                ret.add(mwf);
+            }
+        }
+        return ret;
+    }
+
+    class MatchWf {
+        String wf;
+        List<Word> bases;
+        MatchWf matchingWf;
+        Word base;
+        Word trBase;
+
+        MatchWf(String wf, List bases) {
+            this.matchingWf = null;
+            this.wf = wf;
+            this.bases = bases;
+        }
+    }
 }
--- src/main/java/vlad/aligner/wuo/TrExtractor.java
+++ src/main/java/vlad/aligner/wuo/TrExtractor.java
@@ -1,11 +1,7 @@
 package vlad.aligner.wuo;
 
 import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import vlad.aligner.wuo.db.DAO;
@@ -32,17 +28,14 @@ public class TrExtractor {
    }
 
    public TrExtractor(Connection con) {
-      this.translator = new DbTranslator(con);
-      List errorList = this.translator.checkDB(enLoc, ukLoc);
+      translator = new DbTranslator(con);
+      List<String> errorList = translator.checkDB(enLoc, ukLoc);
       if (errorList != null && errorList.size() > 0) {
-         Iterator var3 = errorList.iterator();
-
-         while(var3.hasNext()) {
-            String s = (String)var3.next();
+         for (String s : errorList) {
             System.err.println(s);
          }
-
-         throw new RuntimeException("ERROR: DB-inconsistencies detected.");
+         System.err.println("ERROR: DB-inconsistencies detected.");
+         return;
       }
    }
 
@@ -57,50 +50,41 @@ public class TrExtractor {
    public void extractTranslations(String ukSentStr, String enSentStr) {
       MatchSentence ukSent = new MatchSentence(ukSentStr, ukLoc, this.translator);
       MatchSentence enSent = new MatchSentence(enSentStr, enLoc, this.translator);
+      for (MatchSentence.MatchWf enWf : enSent.getMatchWfList()) {
+
+      }
+
       Iterator var5 = enSent.getMatchWfList().iterator();
 
       while(true) {
          MatchSentence.MatchWf enWf;
-         ArrayList ukTrList;
-         HashMap trMap;
-         Iterator var13;
-         Word enWord;
-         List ukMatches;
+         Map<Word, List<Word>> trMap;
+         List<Word> ukTrList;
+         List<MatchSentence.MatchWf> ukMatches;
          do {
-            if (!var5.hasNext()) {
-               var5 = enSent.getMatchWfList().iterator();
-
-               while(var5.hasNext()) {
-                  enWf = (MatchSentence.MatchWf)var5.next();
-                  if (enWf.matchingWf != null) {
+            if (!var5.hasNext()) {  // end of loop while(true)
+               for (MatchSentence.MatchWf enWf1 : enSent.getMatchWfList()) {
+                  if (enWf1.matchingWf != null) {
                      if (this.getTrStats() != null) {
-                        this.getTrStats().add("" + enWf.base.getId() + "_" + enWf.trBase.getId());
+                        this.getTrStats().add("" + enWf1.base.getId() + "_" + enWf1.trBase.getId());
                      }
-
-                     System.out.println(enWf.wf + " , " + enWf.base + " -> " + enWf.trBase + ", " + enWf.matchingWf.wf);
+                     System.out.println(enWf1.wf + " , " + enWf1.base + " -> " + enWf1.trBase + ", " + enWf1.matchingWf.wf);
                   }
                }
-
                return;
             }
 
             enWf = (MatchSentence.MatchWf)var5.next();
             ukTrList = new ArrayList();
             trMap = new HashMap();
-            Iterator var9 = enWf.bases.iterator();
-
-            while(var9.hasNext()) {
-               enWord = (Word)var9.next();
-               List enWordList = new ArrayList();
+            for (Word enWord : enWf.bases) {
+               List<Word> enWordList = new ArrayList();
                enWordList.add(enWord);
-               List trList = this.translator.getTranslation(enWordList, enLoc, ukLoc);
+               List<Word> trList = translator.getTranslation(enWordList, enLoc, ukLoc);
                trMap.put(enWord, trList);
-               var13 = trList.iterator();
-
-               while(var13.hasNext()) {
-                  enWord = (Word)var13.next();
-                  if (!ukTrList.contains(enWord)) {
-                     ukTrList.add(enWord);
+               for (Word ukWord : trList) {
+                  if (!ukTrList.contains(ukWord)) {
+                     ukTrList.add(ukWord);
                   }
                }
             }
@@ -108,33 +92,29 @@ public class TrExtractor {
             ukMatches = ukSent.findMatches(ukTrList);
          } while(ukMatches.size() != 1);
 
-         MatchSentence.MatchWf ukMatch = (MatchSentence.MatchWf)ukMatches.get(0);
+         MatchSentence.MatchWf ukMatch = ukMatches.get(0);
          enWf.matchingWf = ukMatch;
          ukMatch.matchingWf = enWf;
-         Stream var10000 = ukTrList.stream();
-         List var10001 = ukMatch.bases;
-         var10001.getClass();
-         List matchUkWordList = (List)var10000.filter(var10001::contains).collect(Collectors.toList());
+         //Stream var10000 = ukTrList.stream();
+         //List var10001 = ukMatch.bases;
+         //var10001.getClass();
+         List<Word> matchUkWordList = ukTrList.stream().filter(ukMatch.bases::contains).collect(Collectors.toList());
          if (matchUkWordList.size() > 1) {
             System.out.println("More then one uk word for (" + enWf.wf + "," + enWf.matchingWf.wf + ")");
          }
 
-         enWf.trBase = (Word)matchUkWordList.get(0);
-         List matchEnWordList = new ArrayList();
-         var13 = trMap.keySet().iterator();
-
-         while(var13.hasNext()) {
-            enWord = (Word)var13.next();
-            if (((List)trMap.get(enWord)).contains(enWf.trBase)) {
+         enWf.trBase = matchUkWordList.get(0);
+         List<Word> matchEnWordList = new ArrayList();
+         for (Word enWord : trMap.keySet()) {
+            if ((trMap.get(enWord)).contains(enWf.trBase)) {
                matchEnWordList.add(enWord);
             }
          }
-
          if (matchEnWordList.size() > 1) {
             System.out.println("More then one en word for (" + enWf.wf + "," + enWf.matchingWf.wf + ")");
          }
 
-         enWf.base = (Word)matchEnWordList.get(0);
+         enWf.base = matchEnWordList.get(0);
       }
    }
 }

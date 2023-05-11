440c4f65e530b50ade9f96e539a91146c598ab65
vlddev <volodymyr.vlad@yahoo.com>
2023-05-11 10:51:30 +0200

    cleanup

--- src/main/java/vlad/text/LineSentenceReader.java
+++ src/main/java/vlad/text/LineSentenceReader.java
@@ -4,12 +4,7 @@ import java.util.Scanner;
 
 public class LineSentenceReader {
 
-	//code of unicode character '…' is \u2026
-	private static String END_OF_SENTENCE = ".!?"+'\u2026';
-	private static String[] NOT_THE_END = {"Mrs.","Dr.","Mr.","...","..","хв."};
-
     private Scanner scanner;
-	private int pos = 0;
 
 	public LineSentenceReader(String text){
 		scanner = new Scanner(text);
